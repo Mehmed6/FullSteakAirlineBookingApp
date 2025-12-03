4b43c0a7b16ec7bf84951530974af61e5fb2e3b5
Mehmet Dogan <mehmed_dogan6@hotmail.com>
2025-12-03 02:05:44 +0100

    Implement centralized exception handling with custom security handlers
    Added custom exception classes (BadRequestException, NotFoundException) and a
    GlobalExceptionHandler to standardize API error responses. Also introduced
    CustomAuthenticationEntryPoint and CustomAccessDenialHandler for consistent
    401/403 handling in Spring Security.

---
+++ src/main/java/com/doganmehmet/AirlineBookingApp/exception/GlobalExceptionHandler.java
@@ -0,0 +1,44 @@
+package com.doganmehmet.AirlineBookingApp.exception;
+
+import com.doganmehmet.AirlineBookingApp.dto.response.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Response<?>> handleAllUnknownExceptions(Exception ex)
+    {
+        var response = Response.builder()
+                .message(ex.getMessage())
+                .statusCode(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<Response<?>> handleNotFoundException(Exception ex)
+    {
+        var response = Response.builder()
+                .message(ex.getMessage())
+                .statusCode(HttpStatus.NOT_FOUND.value())
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<Response<?>> handleBadRequestException(Exception ex)
+    {
+        var response = Response.builder()
+                .message(ex.getMessage())
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+}
