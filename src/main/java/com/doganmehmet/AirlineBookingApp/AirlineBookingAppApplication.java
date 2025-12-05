fb254788a3f513fb4d398516729442dde402c9e9
Mehmet Dogan <mehmed_dogan6@hotmail.com>
2025-12-05 14:19:22 +0100

    Update AirlineBookingAppApplication.java

--- src/main/java/com/doganmehmet/AirlineBookingApp/AirlineBookingAppApplication.java
+++ src/main/java/com/doganmehmet/AirlineBookingApp/AirlineBookingAppApplication.java
@@ -2,12 +2,14 @@ package com.doganmehmet.AirlineBookingApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
+@EnableJpaAuditing
 public class AirlineBookingAppApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AirlineBookingAppApplication.class, args);
 	}
-
 }
