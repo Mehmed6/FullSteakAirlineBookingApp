2db03c3d31c23e78fd0006f12fa473af38e3e6a0
Mehmet Dogan <mehmed_dogan6@hotmail.com>
2025-12-17 18:03:42 +0100

    Update App.jsx

--- AirlineFrontend/src/App.jsx
+++ AirlineFrontend/src/App.jsx
@@ -12,6 +12,12 @@ import UpdateProfilePage from "./components/profile/UpdateProfilePage.jsx";
 import BookingPage from "./components/pages/BookingPage.jsx";
 import BookingDetailsPage from "./components/pages/BookingDetailsPage.jsx";
 import {RouteGuard} from "./services/RouteGuard.jsx";
+import AdminDashboardPage from "./components/admin/AdminDashboardPage.jsx";
+import AdminBookingDetailsPage from "./components/admin/AdminBookingDetailsPage.jsx";
+import AdminFlightDetailsPage from "./components/admin/AdminFlightDetailsPage.jsx";
+import AddEditAirportPage from "./components/admin/AddEditAirportPage.jsx";
+import AddFlightPage from "./components/admin/AddFlightPage.jsx";
+import SpecialRegistration from "./components/admin/SpecialRegistration.jsx";
 
 function App() {
 
@@ -29,6 +35,15 @@ function App() {
 
             <Route path="/book-flight/:id" element={<RouteGuard allowedRoles={["CUSTOMER", "ADMIN", "PILOT"]} element={<BookingPage/>}/>}/>
             <Route path="/booking/:id" element={<RouteGuard allowedRoles={["CUSTOMER", "ADMIN", "PILOT"]} element={<BookingDetailsPage/>}/>}/>
+
+            <Route path="/admin" element={<RouteGuard allowedRoles={["ADMIN", "PILOT"]} element={<AdminDashboardPage/>}/>}/>
+            <Route path="/admin/booking/:id" element={<RouteGuard allowedRoles={["ADMIN", "PILOT"]} element={<AdminBookingDetailsPage/>}/>}/>
+            <Route path="/admin/flight/:id" element={<RouteGuard allowedRoles={["ADMIN", "PILOT"]} element={<AdminFlightDetailsPage/>}/>}/>
+
+            <Route path="/add-airport" element={<RouteGuard allowedRoles={["ADMIN"]} element={<AddEditAirportPage/>}/>}/>
+            <Route path="/edit-airport/:id" element={<RouteGuard allowedRoles={["ADMIN"]} element={<AddEditAirportPage/>}/>}/>
+            <Route path="/add-flight" element={<RouteGuard allowedRoles={["ADMIN", "PILOT"]} element={<AddFlightPage/>}/>}/>
+            <Route path="/special-register" element={<RouteGuard allowedRoles={["ADMIN"]} element={<SpecialRegistration/>}/>}/>
 
             <Route path="*" element={<Navigate to="/home"/>}/>
         </Routes>
